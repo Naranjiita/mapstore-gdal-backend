720adc1f75e8217f831d7b4d1e87c9f3af00c7e2
Emily Romero <romero.emily.225@gmail.com>
2025-02-28 00:42:29 -0500

    Changed shutdown

--- app/main.py
+++ app/main.py
@@ -2,25 +2,27 @@ from fastapi import FastAPI
 from app.routes import raster
 from fastapi.middleware.cors import CORSMiddleware
 import signal
-import sys
+import asyncio
 
 app = FastAPI(title="MapStore GDAL Backend")
 
 # Habilitar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permitir todas las solicitudes. Puedes cambiarlo por ["http://localhost:8081"]
+    allow_origins=["*"],  # Permitir todas las solicitudes
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE)
     allow_headers=["*"],  # Permitir todos los encabezados
 )
 
 app.include_router(raster.router, prefix="/api", tags=["Raster Processing"])
-def shutdown():
-    print("Cerrando servicio GDAL...")
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, lambda s, f: shutdown())
 
 @app.get("/")
 def root():
     return {"message": "GDAL API funcionando 🚀"}
+
+def shutdown():
+    print("Cerrando servicio GDAL...")
+    loop = asyncio.get_event_loop()
+    loop.stop()  # 📌 Detener el bucle de eventos sin generar SystemExit
+
+signal.signal(signal.SIGTERM, lambda s, f: shutdown())
