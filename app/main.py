4d1b2706e180302bdbe4b6f4562d77edede2f823
Emily Romero <romero.emily.225@gmail.com>
2025-02-28 01:25:59 -0500

    There is no way

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
-import asyncio
-import signal
-import sys
 from fastapi import FastAPI
 from app.routes import raster
 from fastapi.middleware.cors import CORSMiddleware
+import signal
+import asyncio
 
 app = FastAPI(title="MapStore GDAL Backend")
 
 # Habilitar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],  # Permitir todas las solicitudes
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],  # Permitir todos los encabezados
 )
 
 app.include_router(raster.router, prefix="/api", tags=["Raster Processing"])
@@ -21,31 +20,3 @@ app.include_router(raster.router, prefix="/api", tags=["Raster Processing"])
 def root():
     return {"message": "GDAL API funcionando 🚀"}
 
-# 🔹 Evento de apagado limpio
-@app.on_event("shutdown")
-async def shutdown_event():
-    print("*** Servidor GDAL FastAPI iniciado. ***")
-    print("Cerrando servicio GDAL...")
-
-    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
-    for task in tasks:
-        task.cancel()
-
-    try:
-        await asyncio.gather(*tasks, return_exceptions=True)  # 🔹 Esperar que todas las tareas terminen
-    except asyncio.CancelledError:
-        pass  # Ignorar `CancelledError` al cerrar el servicio
-
-    print("--- Todas las tareas han sido canceladas. Servicio cerrado correctamente.")
-
-# 🔹 Capturar señales SIGTERM y SIGINT correctamente sin interrumpir asyncio
-def handle_exit(signum, frame):
-    print(f"Recibida señal {signum}, cerrando...")
-    try:
-        loop = asyncio.get_running_loop()
-        loop.create_task(shutdown_event())  # Ejecutar el cierre limpio
-    except RuntimeError:
-        asyncio.run(shutdown_event())
-
-signal.signal(signal.SIGTERM, handle_exit)
-signal.signal(signal.SIGINT, handle_exit)
