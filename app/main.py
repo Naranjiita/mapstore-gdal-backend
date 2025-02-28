722107e5d49e9568bbd29cb1a7191c110ef9ed1a
Emily Romero <romero.emily.225@gmail.com>
2025-02-28 00:46:33 -0500

    Improve cycle of life FastAPI

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 from app.routes import raster
 from fastapi.middleware.cors import CORSMiddleware
-import signal
 import asyncio
 
 app = FastAPI(title="MapStore GDAL Backend")
 
+#  Manejo del ciclo de vida de FastAPI
+@app.on_event("startup")
+async def startup_event():
+    print("---Servidor GDAL FastAPI iniciado.")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("Cerrando servicio GDAL...")
+    loop = asyncio.get_event_loop()
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+    [task.cancel() for task in tasks]  #  Cancelar todas las tareas pendientes
+    await asyncio.gather(*tasks, return_exceptions=True)  #  Esperar que todas se terminen
+    loop.stop()  #  Detener el bucle de eventos correctamente
+
 # Habilitar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permitir todas las solicitudes
-    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE)
-    allow_headers=["*"],  # Permitir todos los encabezados
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(raster.router, prefix="/api", tags=["Raster Processing"])
@@ -19,10 +32,3 @@ app.include_router(raster.router, prefix="/api", tags=["Raster Processing"])
 @app.get("/")
 def root():
     return {"message": "GDAL API funcionando 🚀"}
-
-def shutdown():
-    print("Cerrando servicio GDAL...")
-    loop = asyncio.get_event_loop()
-    loop.stop()  # 📌 Detener el bucle de eventos sin generar SystemExit
-
-signal.signal(signal.SIGTERM, lambda s, f: shutdown())
