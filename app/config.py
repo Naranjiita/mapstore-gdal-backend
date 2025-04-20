8bcb78204da920135e9c78442ff5521e3c7af5c1
Emily Romero <romero.emily.225@gmail.com>
2025-04-19 22:42:51 -0500

    Updated requirements and corrected errors

--- app/config.py
+++ app/config.py
@@ -5,4 +5,5 @@ load_dotenv()  #  .env
 
 GEONETWORK_USER = os.getenv("GEONETWORK_USER")
 GEONETWORK_PASSWORD = os.getenv("GEONETWORK_PASSWORD")
+GEONETWORK_SERVER = os.getenv("GEONETWORK_SERVER")
 GDAL_CACHEMAX = os.getenv("user","password")  # valor por defecto si no se define
